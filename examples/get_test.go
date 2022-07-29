2be5c63d054596730641f9958e2d0863d4d9f8bc
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2022-07-29 14:04:37 -0700

    L26 Defined the Mock Struct

--- examples/get_test.go
+++ examples/get_test.go
@@ -1,17 +1,46 @@
 package examples
 
 import (
-	"fmt"
+	"errors"
+	"net/http"
 	"testing"
+
+	"github.com/rmacdiarmid/myhttp/gohttp"
 )
 
-func TestGet(t *testing.T) {
-	// Initialization:
+func TestGetEndpoints(t *testing.T) {
+	t.Run("TestErrorFetchingFromGithub", func(t *testing.T) {
+		//Initialization:
+		mock := gohttp.Mock{
+			Method: http.MethodGet,
+			Url:    "https://api.github.com",
+			Error:  errors.New("timeout getting github endpoints"),
+		}
+		//Execution:
+		endpoints, err := GetEndpoints()
+	})
 
-	//Execution:
-	endpoints, err := GetEndpoints()
+	t.Run("TestErrorUnmarshalResponseBody", func(t *testing.T) {
+		//Initialization:
+		mock := gohttp.Mock{
+			Method:             http.MethodGet,
+			Url:                "https://api.github.com",
+			ResponseStatusCode: http.StatusOK,
+			ResponseBody:       {"current_user_url": 123},
+		}
+		//Execution:
+		endpoints, err := GetEndpoints()
+	})
 
-	//Validation:
-	fmt.Println(err)
-	fmt.Println(endpoints)
+	t.Run("TestNoError", func(t *testing.T) {
+		// Initialization:
+		mock := gohttp.Mock{
+			Method:             http.MethodGet,
+			Url:                "https://api.github.com",
+			ResponseStatusCode: http.StatusOK,
+			ResponseBody:       {"current_user_url": "https://api.github.com/user"},
+		}
+		//Execution:
+		endpoints, err := GetEndpoints()
+	})
 }
